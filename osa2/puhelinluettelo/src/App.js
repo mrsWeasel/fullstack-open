253caff2afc2bd49e39486df44190debdd178fcd
Heino Laura <laura.2.heino@sok.fi>
2021-10-17 17:29:20 +0300

    Virhe- ja onnistumisilmoitukset

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -3,12 +3,18 @@ import personService from './services/persons'
 import PersonForm from './components/personForm'
 import FilteringForm from './components/filteringForm'
 import Persons from './components/persons'
+import Notification from './components/notification'
+
 
 const App = () => {
+  const TYPENONE = ''
+  const TYPEERROR = 'error'
+  const TYPESUCCESS = 'success'
   const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newPhonenumber, setNewPhonenumber] = useState('')
   const [filteringCondition, setFilteringCondition] = useState('')
+  const [status, setStatus] = useState({ type : TYPENONE, message : '' })
 
   useEffect(() => {
     personService.getAll().then((response) => {
@@ -49,6 +55,10 @@ const App = () => {
         setPersons(persons.concat(response.data))
         setNewName('')
         setNewPhonenumber('')
+        setStatus({ type : TYPESUCCESS, message : 'New person was added successfully.'})
+        setTimeout(() => {
+          setStatus({ type : TYPENONE, message : ''})
+        }, 4000)
       })
       return
     }
@@ -67,9 +77,16 @@ const App = () => {
         setPersons(
           persons.map((p) => (p.id === updatedPerson.id ? updatedPerson : p))
         )
+        setStatus({ type : TYPESUCCESS, message : `Phonenumber for ${person.name} was successfully updated`})
+        setTimeout(() => {
+          setStatus({ type : TYPENONE, message : ''})
+        }, 4000)
       })
       .catch((error) => {
-        alert(error)
+        setStatus({ type : TYPEERROR, message : `There was an error updating phonenumber of ${person.name}.`})
+        setTimeout(() => {
+          setStatus({ type : TYPENONE, message : ''})
+        }, 4000)
       })
       return
     }
@@ -87,14 +104,23 @@ const App = () => {
       .remove(id)
       .then((response) => {
         setPersons(persons.filter((p) => p.id !== id))
+        setStatus({ type : TYPESUCCESS, message : 'Person was successfully removed.'})
+        setTimeout(() => {
+          setStatus({ type : TYPENONE, message : ''})
+        }, 4000)
       })
       .catch((error) => {
-        alert(error)
+        setStatus({ type : TYPEERROR, message : 'There was an error deleting the person.'})
+        setTimeout(() => {
+          setStatus({ type : TYPENONE, message : ''})
+        }, 4000)
+        
       })
   }
 
   return (
     <div>
+      {status.type !== TYPENONE && <Notification type={status.type} message={status.message}/> }
       <h2>Phonebook</h2>
       <PersonForm
         handleAddNewPerson={handleAddNewPerson}
---
+++ osa2/puhelinluettelo/src/components/notification.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+const Notification = ({ type, message }) => {
+    const TYPEERROR = 'error'
+    const TYPESUCCESS = 'success'
+    let backgroundColor = ''
+    
+    if (type === TYPEERROR) {
+        backgroundColor = 'red'
+    }
+
+    if (type === TYPESUCCESS) {
+        backgroundColor = 'green'
+    }
+
+    const styles = {
+      position: 'fixed',
+      bottom: 10,
+      left: 10,
+      right: 10,
+      padding: 16,
+      backgroundColor: backgroundColor,
+      borderRadius: 4,
+      transition: 'all 1s ease-in-out'
+    }
+  
+    return (
+      <div style={styles}>
+        {message}
+      </div>
+    )
+  }
+
+export default Notification
