a862266c22b56f9255a7793fae1c7fac772075d2
Laura Heino <lauraheino@Lauras-MBP.home>
2022-09-12 22:17:50 +0300

    blogilistan yksikkötestejä 4.4

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -2,6 +2,15 @@ const dummy = blogs => {
     return 1
 }
 
+const totalLikes = blogs => {
+    if (!blogs || blogs.length < 1) return 0
+    
+    const total = blogs.reduce((a, b) => ({ likes : a.likes + b.likes }), {likes : 0})
+
+    return total.likes
+}
+
 module.exports = {
     dummy,
+    totalLikes,
 }
