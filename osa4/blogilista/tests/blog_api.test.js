3e5f7fb294ff820a9a5f2572adb255eb2e1aec33
Laura Heino <lauraheino@Lauras-MBP.home>
2022-09-17 14:28:30 +0300

    4.8 testejä apille

---
+++ osa4/blogilista/tests/blog_api.test.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const Blog = require('../models/blog')
+const { listWithSixBlogs } = require('./test_data')
+
+const api = supertest(app)
+
+beforeEach(async () => {
+    await Blog.deleteMany({})
+
+    for (let i = 0; i < listWithSixBlogs.length; i++) {
+        let blogObject = new Blog(listWithSixBlogs[i])
+        await blogObject.save()
+    }
+})
+
+test('blogs are returned as json', async () => {
+    await api  
+        .get('/api/blogs')
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+})
+
+test('there are 6 blogs', async () => {
+    const response = await api.get('/api/blogs')
+
+    expect(response.body).toHaveLength(6)
+})
+
+afterAll(() => {
+    mongoose.connection.close()
+})
