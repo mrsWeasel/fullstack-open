baddc628fd356109ae5ad6ff3bf8bca11c0a7b6e
Laura Heino <lauramainen@gmail.com>
2022-10-10 22:46:07 +0300

    5.21

--- osa5/bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ osa5/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -60,5 +60,42 @@ describe('Blog app', function () {
         cy.contains('Blog created by Cypress').parent().contains('Like').click()
         cy.contains('Blog created by Cypress').parent().contains('Likes: 1')
     })
+    it('A blog can be deleted by creator', function () {
+      cy.request({
+        url: 'http://localhost:3003/api/blogs',
+        method: 'POST',
+        body: {title: 'Blog created by Cypress', author: 'Cypress', url: 'https://docs.cypress.io/guides/overview/why-cypress#In-a-nutshell'},
+        headers: {
+          'Authorization': `bearer ${JSON.parse(localStorage.getItem('loggedInBlogUser')).token}`
+        }
+      })  
+      cy.visit('http://localhost:3000')
+      cy.contains('Blog created by Cypress').contains('View').click()
+      cy.contains('Blog created by Cypress').parent().contains('Remove').click()
+      cy.get('html').should('not.contain', 'Blog created by Cypress')
+  })
+  it('A blog cannot be deleted by random user', function() {
+    cy.request('POST', 'http://localhost:3003/api/users', { name: 'Sanna Muuronen', username: 'santeli', password: 'iletnas' })
+    // create blog with Manna Puuronen and logout
+    cy.request({
+      url: 'http://localhost:3003/api/blogs',
+      method: 'POST',
+      body: {title: 'Blog created by Cypress', author: 'Cypress', url: 'https://docs.cypress.io/guides/overview/why-cypress#In-a-nutshell'},
+      headers: {
+        'Authorization': `bearer ${JSON.parse(localStorage.getItem('loggedInBlogUser')).token}`
+      }
+    })
+    cy.visit('http://localhost:3000')
+    cy.contains('Blog created by Cypress')
+    cy.contains('Logout').click()
+     // login as Sanna Muuronen and try to remove blog
+    cy.get('#username').type('santeli')
+    cy.get('#password').type('iletnas')
+    cy.get('#submitLogin').click()
+    cy.contains('Sanna Muuronen')
+    cy.contains('Blog created by Cypress').contains('View').click()
+    cy.contains('Blog created by Cypress').parent().contains('Remove').click()
+    cy.contains('Blog created by Cypress')
+  })
   })
 })
