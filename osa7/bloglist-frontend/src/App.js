ece64a7d49dc6cda0ad6eadb1941375b410a53ba
Laura Heino <lauramainen@gmail.com>
2022-11-17 21:51:39 +0200

    Routerin aloitus

--- osa7/bloglist-frontend/src/App.js
+++ osa7/bloglist-frontend/src/App.js
@@ -1,19 +1,15 @@
-import { useState, useEffect, useRef } from 'react'
-import Blog from './components/Blog'
+import { useState, useEffect } from 'react'
 import LoginForm from './components/LoginForm'
-import CreateBlogForm from './components/CreateBlogForm'
-import Togglable from './components/Togglable'
 import loginService from './services/login'
-import blogService from './services/blogs'
+import { Route, Routes } from 'react-router-dom'
+import Blogs from './components/Blogs'
 
 const App = () => {
-  const [blogs, setBlogs] = useState([])
   const [user, setUser] = useState(null)
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
   const [successMessage, setSuccessMessage] = useState('')
   const [errorMessage, setErrorMessage] = useState('')
-  const createBlogFormRef = useRef()
 
   const handleShowErrorMessage = (message) => {
     setErrorMessage(message)
@@ -30,35 +26,12 @@ const App = () => {
   }
 
   useEffect(() => {
-    blogService.getAllBlogs().then((blogs) => {
-      blogs.sort((a, b) => {
-        return b.likes - a.likes
-      })
-      setBlogs(blogs)
-    })
-  }, [])
-
-  useEffect(() => {
     const loggedInBlogUser = window.localStorage.getItem('loggedInBlogUser')
     if (!loggedInBlogUser) return
 
     const user = JSON.parse(loggedInBlogUser)
     setUser(user)
   }, [])
-
-  const handleCreateBlog = async (blog) => {
-    const data = await blogService.createBlog(blog)
-
-    if (data.errorMessage) {
-      console.log(data.errorMessage)
-      handleShowErrorMessage(`Error creating blog: ${data.errorMessage}`)
-      return
-    }
-    createBlogFormRef.current.toggleVisibility()
-    handleShowSuccessMessage(`Blog '${data.title}' created successfully!`)
-    const updatedBlogs = [...blogs, data]
-    setBlogs(updatedBlogs)
-  }
 
   const handleLogin = async (event) => {
     event.preventDefault()
@@ -97,22 +70,6 @@ const App = () => {
     }
   }
 
-  const handleDelete = (blog) => async () => {
-    console.log('delete', blog?.title)
-    const data = await blogService.deleteBlog(blog)
-
-    if (data.errorMessage) {
-      console.log(data.errorMessage)
-      handleShowErrorMessage(
-        `Error removing ${blog.title}: ${data.errorMessage}`
-      )
-      return
-    }
-
-    const updatedBlogs = blogs.filter((b) => b.id !== blog.id)
-    setBlogs(updatedBlogs)
-  }
-
   const renderError = () => {
     return (
       <div style={{ border: '2px solid red', padding: 16 }}>{errorMessage}</div>
@@ -127,36 +84,26 @@ const App = () => {
     )
   }
 
-  const renderBlogs = () => {
-    return (
-      <div>
-        <h2>Blogs</h2>
-        <p>{user.name} logged in</p>
-        <button onClick={handleLogout}>Logout</button>
-        {blogs.map((blog) => (
-          <Blog key={blog.id} blog={blog} handleDelete={handleDelete(blog)} />
-        ))}
-      </div>
-    )
-  }
-
   return (
     <div>
       {errorMessage && renderError()}
       {successMessage && renderSuccess()}
       {user ? (
         <>
-          {renderBlogs()}
-          <Togglable
-            buttonLabel="Create new blog"
-            useCancel={true}
-            ref={createBlogFormRef}>
-            <CreateBlogForm
-              handleShowErrorMessage={handleShowErrorMessage}
-              handleShowSuccessMessage={handleShowSuccessMessage}
-              handleCreateBlog={handleCreateBlog}
+          <h2>Blogs</h2>
+          <p>{user.name} logged in</p>
+          <button onClick={handleLogout}>Logout</button>
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <Blogs
+                  handleShowErrorMessage={handleShowErrorMessage}
+                  handleShowSuccessMessage={handleShowSuccessMessage}
+                />
+              }
             />
-          </Togglable>
+          </Routes>
         </>
       ) : (
         <LoginForm
---
+++ osa7/bloglist-frontend/src/components/Blogs.js
@@ -0,0 +1,69 @@
+import { useState, useEffect, useRef } from 'react'
+import Blog from './Blog'
+import CreateBlogForm from './CreateBlogForm'
+import Togglable from './Togglable'
+import blogService from '../services/blogs'
+
+const Blogs = ({ handleShowErrorMessage, handleShowSuccessMessage }) => {
+  const createBlogFormRef = useRef()
+  const [blogs, setBlogs] = useState([])
+
+  useEffect(() => {
+    blogService.getAllBlogs().then((blogs) => {
+      blogs.sort((a, b) => {
+        return b.likes - a.likes
+      })
+      setBlogs(blogs)
+    })
+  }, [])
+
+  const handleCreateBlog = async (blog) => {
+    const data = await blogService.createBlog(blog)
+
+    if (data.errorMessage) {
+      console.log(data.errorMessage)
+      handleShowErrorMessage(`Error creating blog: ${data.errorMessage}`)
+      return
+    }
+    createBlogFormRef.current.toggleVisibility()
+    handleShowSuccessMessage(`Blog '${data.title}' created successfully!`)
+    const updatedBlogs = [...blogs, data]
+    setBlogs(updatedBlogs)
+  }
+
+  const handleDelete = (blog) => async () => {
+    console.log('delete', blog?.title)
+    const data = await blogService.deleteBlog(blog)
+
+    if (data.errorMessage) {
+      console.log(data.errorMessage)
+      handleShowErrorMessage(
+        `Error removing ${blog.title}: ${data.errorMessage}`
+      )
+      return
+    }
+
+    const updatedBlogs = blogs.filter((b) => b.id !== blog.id)
+    setBlogs(updatedBlogs)
+  }
+
+  return (
+    <div>
+      {blogs.map((blog) => (
+        <Blog key={blog.id} blog={blog} handleDelete={handleDelete(blog)} />
+      ))}
+      <Togglable
+        buttonLabel="Create new blog"
+        useCancel={true}
+        ref={createBlogFormRef}>
+        <CreateBlogForm
+          handleShowErrorMessage={handleShowErrorMessage}
+          handleShowSuccessMessage={handleShowSuccessMessage}
+          handleCreateBlog={handleCreateBlog}
+        />
+      </Togglable>
+    </div>
+  )
+}
+
+export default Blogs
